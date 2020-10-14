f867b17f0b225a3adb3d0c47d39dcbe6a34261b6
Alessandra <alessandra.guedes@outlook.com>
2020-10-14 20:33:21 -0300

    - Alteração na arquitetura do projeto, pasta cadastra usuario.

--- InterfaceGraphic/TelaCadastraUsuario/src/CadastroUsuario/cadastrar.java
+++ InterfaceGraphic/TelaCadastraUsuario/src/CadastroUsuario/cadastrar.java
@@ -1,23 +1,22 @@
-package sample;
+package CadastroUsuario;
 
-import CadastroUsuario.cadastrar;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+public class cadastrar extends Application {
 
-cadastrar tela = new cadastrar();
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("cadastrar.fxml"));
+        primaryStage.setTitle("Cadastre-se");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 }
