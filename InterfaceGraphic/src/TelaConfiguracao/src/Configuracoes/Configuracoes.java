303f960eeadfbdce7e843dd3b7446be19b957101
Guilherme Mecenero <guilherme.machado@saebrasil.org.br>
2020-10-21 18:39:41 -0300

    Criação da Tela de Configurações

---
+++ InterfaceGraphic/src/TelaConfiguracao/src/Configuracoes/Configuracoes.java
@@ -0,0 +1,16 @@
+package TelaConfiguracao.src.Configuracoes;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Configuracoes extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
